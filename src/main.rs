53553ee0c9916ef3892d77babbb87f7ba2b07ad4
Felix Pape <Felix.Pape@vartijat.de>
2021-01-02 01:41:37 +0100

    Fixed GPIO Handling

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,18 @@
 extern crate clap;
 
 use clap::App;
-use rumqttc::{MqttOptions, Client, QoS, Incoming};
+use rumqttc::{Client, Incoming, MqttOptions, QoS};
 use sysfs_gpio::{Direction, Edge, Pin};
 
-use std::time::SystemTime;
 use std::path::Path;
-use std::thread;
 use std::sync::{Arc, Mutex};
+use std::thread;
+use std::time::SystemTime;
 
 mod hsmr_spaceapi;
 use hsmr_spaceapi::Tuerstatus;
 
-fn main() { 
+fn main() {
     // Command line arguments
     // TODO: Additional parameters. E.g.: broker host, broker port, mqtt keep alive, mqtt topic, verbose switch, connected pin
     let yaml = load_yaml!("cli.yml");
@@ -30,41 +30,35 @@ fn main() {
     mqttoptions.set_keep_alive(5);
     let (mut client, connection) = Client::new(mqttoptions, 10);
     client.subscribe("door", QoS::ExactlyOnce).unwrap();
-    
-    // Setup Mutex for communication between MQTT handler and event updater
-    let current_state = Arc::new(Mutex::new(
-        Tuerstatus{
-            door_open: false,
-            timestamp: unixtime_now(),
-            flti_only: None
-        }
-    ));
-    
-    // Spawn Threads
-    spawn_mqtt_handler(
-        connection, 
-        String::from(wikipath), 
-        &current_state
-    );
-    
-    spawn_event_updater(
-        String::from(wikipath),
-        &current_state
-    );
-    
-    // Poll for button state and change state accordingly 
-    // State is only changed via MQTT as the handlers above deal with all the rest
-    switch_interrupt(pin, &current_state, &mut client).expect("Issues during GPIO Handling");
 
+    // Setup Mutex for communication between MQTT handler and event updater
+    let current_state = Arc::new(Mutex::new(Tuerstatus {
+        door_open: false,
+        timestamp: unixtime_now(),
+        flti_only: None,
+    }));
+
+    // Spawn Threads
+    spawn_mqtt_handler(connection, String::from(wikipath), &current_state);
+
+    spawn_event_updater(String::from(wikipath), &current_state);
+
+    // Poll for button state and change state accordingly
+    // State is only changed via MQTT as the handlers above deal with all the rest
+    switch_handling(pin, &current_state, &mut client).expect("Issues during GPIO Handling");
 }
 
 /// Thread handling incoming Tuerstatus mqtt messages and updating spaceapi and website
-fn spawn_mqtt_handler(mut connection: rumqttc::Connection, wikistr: String, current_state: &Arc<Mutex<Tuerstatus>>) -> thread::JoinHandle<()> {
+fn spawn_mqtt_handler(
+    mut connection: rumqttc::Connection,
+    wikistr: String,
+    current_state: &Arc<Mutex<Tuerstatus>>,
+) -> thread::JoinHandle<()> {
     let current_state = current_state.clone();
     thread::spawn(move || {
         let wikipath = Path::new(wikistr.as_str());
-        for notification in connection.iter(){
-            if let rumqttc::Event::Incoming(Incoming::Publish(inc)) = notification.unwrap(){
+        for notification in connection.iter() {
+            if let rumqttc::Event::Incoming(Incoming::Publish(inc)) = notification.unwrap() {
                 let message_str = String::from_utf8(inc.payload.to_vec()).unwrap();
                 if let Ok(result) = serde_json::from_str::<Tuerstatus>(message_str.as_str()) {
                     println!("Tuerstatus: {} at {}", result.door_open, result.timestamp);
@@ -82,12 +76,15 @@ fn spawn_mqtt_handler(mut connection: rumqttc::Connection, wikistr: String, curr
 }
 
 /// Thread checking for new events and updating spaceapi accordingly
-fn spawn_event_updater(wikistr: String, current_state: &Arc<Mutex<Tuerstatus>>) -> thread::JoinHandle<()>{
+fn spawn_event_updater(
+    wikistr: String,
+    current_state: &Arc<Mutex<Tuerstatus>>,
+) -> thread::JoinHandle<()> {
     let current_state = current_state.clone();
     thread::spawn(move || {
         let wikipath = Path::new(wikistr.as_str());
-        loop{
-            thread::sleep(std::time::Duration::from_secs(5*60));
+        loop {
+            thread::sleep(std::time::Duration::from_secs(5 * 60));
             let state = current_state.lock().unwrap();
             hsmr_spaceapi::write_spaceapi(wikipath, &*state);
         }
@@ -95,7 +92,12 @@ fn spawn_event_updater(wikistr: String, current_state: &Arc<Mutex<Tuerstatus>>) 
 }
 
 /// Handles button changes
-fn switch_interrupt(pin:u64, current_state: &Arc<Mutex<Tuerstatus>>, mqtt_client: &mut rumqttc::Client) -> sysfs_gpio::Result<()> {
+/// uses polling in a eternal loop.
+fn switch_handling(
+    pin: u64,
+    current_state: &Arc<Mutex<Tuerstatus>>,
+    mqtt_client: &mut rumqttc::Client,
+) -> sysfs_gpio::Result<()> {
     let current_state = current_state.clone();
     let input = Pin::new(pin);
     input.with_exported(|| {
@@ -103,15 +105,11 @@ fn switch_interrupt(pin:u64, current_state: &Arc<Mutex<Tuerstatus>>, mqtt_client
         input.set_edge(Edge::BothEdges)?;
         let mut poller = input.get_poller()?;
         loop {
-            match poller.poll(2000)? {
-                Some(value) => {
-                    let state = value > 128; // WILD assumption that high is open and high encodes to something above 0.5 * max(u8)
-                    let tuerstatus = current_state.lock().unwrap();
-                    if tuerstatus.door_open != state {
-                        new_door_state(state, mqtt_client);
-                    }
-                }, // TODO: test
-                None => println!("Timeout during switch polling!")
+            if Some(value) = poller.poll(1000 * 60 * 60 * 24 * 7)? {
+                // This timout can be huge. Button changes can be very infrequent
+                let state = value == 1;
+                new_door_state(state, mqtt_client);
+                println("Tuerstatus is now: {}", state);
             }
         }
     })
@@ -120,25 +118,25 @@ fn switch_interrupt(pin:u64, current_state: &Arc<Mutex<Tuerstatus>>, mqtt_client
 /// Shortcut to get current system unixtime as u64
 fn unixtime_now() -> u64 {
     SystemTime::now()
-        .duration_since(
-            SystemTime::UNIX_EPOCH
-        )
+        .duration_since(SystemTime::UNIX_EPOCH)
         .unwrap()
         .as_secs()
 }
 
 /// Change current hackspace state by sending new state with current time via MQTT
-fn new_door_state(state: bool, mqtt_client: &mut rumqttc::Client){
-    let new = Tuerstatus{
+fn new_door_state(state: bool, mqtt_client: &mut rumqttc::Client) {
+    let new = Tuerstatus {
         door_open: state,
         flti_only: Some(false),
-        timestamp: unixtime_now()
+        timestamp: unixtime_now(),
     };
 
-    mqtt_client.publish(
-        "door",
-        QoS::ExactlyOnce,
-        true,
-        serde_json::to_string(&new).unwrap()
-    ).unwrap();
+    mqtt_client
+        .publish(
+            "door",
+            QoS::ExactlyOnce,
+            true,
+            serde_json::to_string(&new).unwrap(),
+        )
+        .unwrap();
 }
